5108dbbde4641f485b3d38cab3f065d5394db93a
Gustavo <gustavorodrigues.grm@gmail.com>
2022-11-10 21:49:41 -0300

    arquivos do projeto

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,28 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/env.json");
+
 module.exports = async (req, res, next) => {
-  res.status(401).json({
-    msg: "Não passará!",
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      erro: "Token não informado!!!",
+    });
+  }
+
+  const [tipo, token] = authorization.split(" ");
+  if (!token) {
+    return res.status(401).json({
+      erro: "Token não enviado!!!",
+    });
+  }
+
+  jwt.verify(token, config.segredo, (err, userInfo) => {
+    if (err) {
+      return res.status(401).json({ erro: "Token Inválido" });
+    }
+    req.body.id = userInfo.id;
   });
+
   next();
 };
